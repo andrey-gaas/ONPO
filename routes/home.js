e2298399d381a67003fb03373520d1c92ee46dd0
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-07-07 14:50:22 +0700

    api fetch

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const fetch = require('node-fetch');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  fetch('http://moodle/webservice/rest/server.php?wstoken=c945ed7e55b3cc709fe449e59ec67fc0&wsfunction=core_webservice_get_site_info&moodlewsrestformat=json')
+    .then( res => res.json() )
+    .then( data => res.render('home', { text: data.sitename }) )
 });
 
 module.exports = router;
