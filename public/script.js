2d69d76ca1239e32455e57b28e3572eee3183d6b
Алексей <alekseynuzdin@gmail.com>
2021-07-26 12:54:34 +0700

    add accordion, filter courses

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ window.onload = () => {
   const swiper = new Swiper(".swiper-container", {
     // Optional parameters
     direction: "horizontal",
+    slidesPerView: 1,
     loop: true,
 
     // If we need pagination
@@ -23,6 +24,7 @@ window.onload = () => {
     },
   });
 
+  //drop menu
   let closeBtn = document.querySelector('#hide_menu')
   let openBtn = document.querySelector('#show_menu')
   let menuMobile = document.querySelector('#drop_menu_mobile')
@@ -34,7 +36,184 @@ window.onload = () => {
     menuMobile.classList.remove('activ')
   } )
 
+  //scrollToTop
+
+  let btnScrollToTop = document.querySelector('#scrollToTop')
+  btnScrollToTop.addEventListener( 'click', ()=>{ 
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  })
+
+  //accordion
+
+  let accordion = document.querySelector('.accordion_list')
+  if(accordion){
+    accordion.addEventListener('click', (e)=>{
+      let targetElem = e.target
+      if(targetElem.className == "title" || targetElem.id == "toggle_show"){
+        let item = targetElem.parentNode.parentNode
+        let panel = targetElem.parentNode.nextElementSibling
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+        item.classList.toggle("active")
   
+      }
+    })
+  }
+  
+
+  //filter
+
+  let chekIncludeCategorie = (card, dataFilter)=>{
+    let dataset = card.dataset.filter
+    let res = false
+    //console.log(dataset);
+    for(let elem of dataFilter){
+      if(dataset.includes(elem)){
+        res = true
+      }
+    }
+    return res
+  }
+
+  let filterBtnList = document.querySelectorAll('.filter_list .filter_item')
+  let filterCardList = document.querySelectorAll('.article_home_courses .card')
+  let filter = document.querySelector('.filter_list')
+  let filterDate = 'all'
+
+  if(filter.style.left === ''){
+    filter.style.left = '0px'
+  }
+
+  let slideFilter = direction => {
+    let left = +filter.style.left.slice(0, -2)
+    let maxScrol = filter.parentNode.offsetWidth - filter.offsetWidth
+    
+    left+= (direction*250)
+    
+
+    if(left > 0){
+      filter.style.transition = '.15s left'
+      filter.style.left = left + 'px'
+      setTimeout( ()=> {
+        filter.style.left = '0px'
+        setTimeout( ()=> {
+          filter.style.transition = null
+        }, 150)
+      }, 150)
+      return
+    }
+
+    if(left < maxScrol){
+      filter.style.transition = '.15s left'
+      filter.style.left = left + 'px'
+      setTimeout( ()=> {
+        filter.style.left = maxScrol + 'px'
+        setTimeout( ()=> {
+          filter.style.transition = null
+        }, 150)
+      }, 150)
+      return
+    }
+    filter.style.transition = '.2s left'
+    filter.style.left = left + 'px'
+    setTimeout( ()=>{filter.style.transition = null}, 200)
+  }
+  let filterBtnControlBack = document.querySelector('.btn_back.btn_filter_control')
+  let filterBtnControlNext = document.querySelector('.btn_next.btn_filter_control')
+
+  filterBtnControlBack.addEventListener('click', () => slideFilter(1))
+  filterBtnControlNext.addEventListener('click', () => slideFilter(-1))
+
+  filter.onmousedown = e => {
+    //console.log(fitlerBtn, filterCardList);
+    let shiftX = e.clientX;
+    
+
+    let maxScrol = filter.parentNode.offsetWidth - filter.offsetWidth
+    let leftStart = filter.style.left.slice(0, -2)
+
+    let onMouseMove = e => {
+      filter.style.left = (+leftStart) + (e.pageX - shiftX )+ 'px';
+    }
+
+    document.addEventListener('mouseup', () => {
+        if(filter.style.left.slice(0, -2) > 0){
+          filter.style.transition = '.2s left'
+          filter.style.left = '0px'
+          setTimeout( ()=>{filter.style.transition = null}, 200)
+        }
+        if(filter.style.left.slice(0, -2) < maxScrol){
+          filter.style.transition = '.2s left'
+          filter.style.left = maxScrol + 'px'
+          setTimeout( ()=>{filter.style.transition = null}, 200)
+        }
+        document.removeEventListener('mousemove', onMouseMove);
+        document.onmouseup = null;
+    });
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    //btn filter
+    if(e.target.nodeName === "BUTTON"){
+      e.target.onmouseup = e => {
+        if((shiftX - e.clientX)**2 <= 225){
+          
+          let targetFilter = e.target.dataset.filter
+          //let indexElem = filterDate.indexOf(targetFilter)
+          
+          filterBtnList.forEach( item => {
+            item.classList.remove('active')
+          })
+          e.target.classList.toggle('active')
+          
+          if(filterDate === targetFilter){
+            filterDate = ''
+            filterCardList.forEach(item => {
+              item.style.display = 'none'
+            })
+            e.target.classList.toggle('active')
+          }else{
+            if(targetFilter === 'all'){
+              filterCardList.forEach(item => {
+                item.style.display = ''
+              })
+            }else{
+              filterCardList.forEach(item => {
+                if(item.dataset.filter.includes(targetFilter)){
+                  item.style.display = ''
+                }else{
+                  item.style.display = 'none'
+                }
+              })
+            }
+            filterDate = targetFilter
+          }
+            // if( indexElem === -1){
+
+            //   filterDate.push(targetFilter)
+            // }else{
+            //   filterDate.splice( indexElem, 1)
+            // }
+
+            // filterCardList.forEach( item => {
+            //   if(chekIncludeCategorie(item,filterDate )){
+            //     item.style.display = ''
+            //   }else{
+            //     item.style.display = 'none'
+            //   }
+            // })
+        }
+      }
+    }
+  }
+
 };
 
 
--- routes/home.js
+++ routes/home.js
@@ -24,6 +24,19 @@ router.get('/', (req, res) => {
       console.log(`http://moodle/webservice/rest/server.php?${TOKEN}&${FORMAT}&${FUNCTION}${paramsStr}`)
       res.render('home', { text: JSON.stringify(data) 
     })} ) */
+    const direction = [{
+      title: `Аспирантура<br/>гпнтб со ран`,
+      href: '#1',
+      img: 'direction3.png',
+    },{
+      title: `Повышение квалификации`,
+      href: '#2',
+      img: 'direction1.png',
+    },{
+      title: `Профессиональная переподготовка`,
+      href: '#3',
+      img: 'direction2.png',
+    }]
 
     const courses = [
       {
@@ -38,6 +51,49 @@ router.get('/', (req, res) => {
         term: '3 месяца',
         price: '1599',
         image: 'https://cdnimg.rg.ru/img/content/186/45/44/iStock-949118068_d_850.jpg',
+        categories:[ 'best', 're'],
+      },
+      {
+        id: 0,
+        title: 'Делопроизводство. Организация документирования управленческой деятельности в организации',
+        stars: 5,
+        reviews: [{}, {}, {}],
+        type: 'Профессиональная переподготовка',
+        discount: 'Скидка 40% до 14 сентября',
+        start: '14 сентября 2021 года',
+        hours: '16 часов',
+        term: '3 месяца',
+        price: '1599',
+        image: 'https://cdnimg.rg.ru/img/content/186/45/44/iStock-949118068_d_850.jpg',
+        categories:['asp',  're'],
+      },
+      {
+        id: 0,
+        title: 'Делопроизводство. Организация документирования управленческой деятельности в организации',
+        stars: 5,
+        reviews: [{}, {}, {}],
+        type: 'Профессиональная переподготовка',
+        discount: 'Скидка 40% до 14 сентября',
+        start: '14 сентября 2021 года',
+        hours: '16 часов',
+        term: '3 месяца',
+        price: '1599',
+        image: 'https://cdnimg.rg.ru/img/content/186/45/44/iStock-949118068_d_850.jpg',
+        categories:[ 'best', 'pop',],
+      },
+      {
+        id: 0,
+        title: 'Делопроизводство. Организация документирования управленческой деятельности в организации',
+        stars: 5,
+        reviews: [{}, {}, {}],
+        type: 'Профессиональная переподготовка',
+        discount: 'Скидка 40% до 14 сентября',
+        start: '14 сентября 2021 года',
+        hours: '16 часов',
+        term: '3 месяца',
+        price: '1599',
+        image: 'https://cdnimg.rg.ru/img/content/186/45/44/iStock-949118068_d_850.jpg',
+        categories:['asp', 'best', ],
       },
       {
         id: 1,
@@ -50,11 +106,12 @@ router.get('/', (req, res) => {
         hours: 'от 16 до 72 часов',
         term: 'от 2 дней до 2 недель',
         price: '2799',
-        image: 'https://lh3.googleusercontent.com/proxy/kxq1w8CbRUdcFt9nH3liIMXWgK0c3Gj9TC_K2Cbv72WqI_aLoH1sKR9EebN0xA-a-B1NhzzRTy8KJhMdv0ls1IOZfbGCCNaK4Bh3PIRYwufCMkzpXpMUowVwZW9liKlBZw',
+        image: 'https://cdnimg.rg.ru/img/content/186/45/44/iStock-949118068_d_850.jpg',
+        categories:['asp', 'up', 're'],
       }
     ];
 
-    res.render('home', { courses });
+    res.render('home', { courses, direction });
 });
 
 module.exports = router;
