27468c7075230039c628be96d2618969ca54f39f
Алексей <alekseynuzdin@gmail.com>
2021-10-04 16:55:25 +0700

    add script in error page

---
+++ public/scripts/error.js
@@ -0,0 +1,8 @@
+import { initBtnScrollToTop, initDropMenu } from "/scripts/utils.js"
+
+window.onload = function() {
+  initDropMenu('#hide_menu', '#show_menu', '#drop_menu_mobile');
+
+  initBtnScrollToTop('#scrollToTop');
+
+};
