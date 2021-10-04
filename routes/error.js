b874109add49bd48a8203b2ee4378d594c2adb00
Алексей <alekseynuzdin@gmail.com>
2021-10-04 15:58:50 +0700

    add error page 404 and 500

--- routes/error.js
+++ routes/error.js
@@ -5,11 +5,27 @@ const router = Router();
 router.get('/', (req,  res) => {
 
   let error = {
-    code:404,
-    text:"Страница не найдена",
-    description:"Страница, на которую вы перешли, не существует. Возможно она была перенесена или ещё не создана.",
+    code:'---',
+    text:"Непредвиденная ошибка",
+    description:"Что-то сломалось.",
   }
-  const errorCode = req.params.code
+  const errorCode = req.query.code
+  
+  if(+errorCode == 500){
+    error = {
+      code:500,
+      text:"Ошибка сервера",
+      description:"На сервере произошла непредвиденная ошибка. <br>Повторите запрос позже, если ошибка повторится сообщете об этом по почте. <br> email: begisheva@gpntbsib.ru",
+    }
+  }
+
+  if(+errorCode == 404){
+    error = {
+      code:404,
+      text:"Страница не найдена",
+      description:"Страница, на которую вы перешли, не существует. <br>Возможно она была перенесена или ещё не создана.",
+    }
+  }
 
 	res.render('error', {
 		title: 'Ошибка',
