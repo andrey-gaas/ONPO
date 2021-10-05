937709180dae57925b3a0e859a6db67c966b89da
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-10-05 17:48:40 +0700

    training api

--- routes/training.js
+++ routes/training.js
@@ -1,58 +1,35 @@
 const { Router } = require('express');
-const Mongo = require('../db');
+const CourseApi = require('../services/Course');
+const TeacherApi = require('../services/Teacher');
+const ReviewApi = require('../services/Review');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	// Получаем список курсов
-	Mongo
-		.courses
-		.find({ type: 'Повышение квалификации' })
-		.toArray((error, courses) => {
-			if (error) {
-				console.log(error.message);
-				console.log('Ошибка в /training при загрузке списка курсов');
-				return res.redirect('/error?code=500');
-			}
+router.get('/', async (req, res) => {
 
-			const formRaioChekboxList = courses.map(course => ({ title:course.title, id: course.id }));
+	try {
+		const courses = await CourseApi.get({ type: 'Повышение квалификации' });
+		const reviews = await ReviewApi.get({});
+		const teachers = await TeacherApi.get({});
 
-			// Список преподавателей
-			Mongo
-				.teachers
-				.find({})
-				.toArray((error, teachers) => {
-					if (error) {
-						console.log(error.message);
-						console.log('Ошибка в /training при загрузке преподавателей');
-						return res.redirect('/error?code=500');
-					}
+		const formRaioChekboxList = courses.map(course => ({ title:course.title, id: course.id }));
 
-					// Список отзывов
-					Mongo
-						.reviews
-						.find({})
-						.toArray((error, reviews) => {
-							if (error) {
-								console.log(error.message);
-								console.log('Ошибка в /training при загрузке отзывов');
-								return res.redirect('/error?code=500');
-							}
-
-							res.render('courses', {
-								title: 'Программы повышения квалификации',
-								description: 'Создание приверженного покупателя, анализируя результаты рекламной кампании, переворачивает жизненный цикл продукции.',
-								script: 'courses.js',
-								headerTitle: 'Образовательные программы',
-								courses,
-								teachers,
-								reviews,
-								formRaioChekboxList,
-								specialOffer: false,
-							});
-						});
-				});
+		res.render('courses', {
+			title: 'Программы повышения квалификации',
+			description: 'Создание приверженного покупателя, анализируя результаты рекламной кампании, переворачивает жизненный цикл продукции.',
+			script: 'courses.js',
+			headerTitle: 'Образовательные программы',
+			courses,
+			teachers,
+			reviews,
+			formRaioChekboxList,
+			specialOffer: false,
 		});
+	} catch(error) {
+		console.log(error.message);
+		console.log('Ошибка в /training');
+		return res.redirect('/error?code=500');
+	}
 });
 
 module.exports = router;
